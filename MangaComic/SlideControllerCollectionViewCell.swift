90816b2957458afb22cbdb1d2d930f4d2c3c309f
datto098 <dat61222@gmail.com>
2024-05-21 17:03:13 +0700

    home screen

---
+++ MangaComic/SlideControllerCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  SlideControllerCollectionViewCell.swift
+//  MangaComic
+//
+//  Created by dattodev on 21/05/2024.
+//
+
+import UIKit
+
+class SlideControllerCollectionViewCell: UICollectionViewCell {
+    
+}
