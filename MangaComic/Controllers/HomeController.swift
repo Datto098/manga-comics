8090b3ebaa509d0d7248ad5cde86b50254fd9dd3
datto098 <dat61222@gmail.com>
2024-05-21 22:21:11 +0700

    home slide banner

--- MangaComic/Controllers/HomeController.swift
+++ MangaComic/Controllers/HomeController.swift
@@ -11,9 +11,10 @@ class HomeController: UIViewController {
 
     @IBOutlet weak var bannerSlideCollectionView: UICollectionView!
     @IBOutlet weak var bannerSlidePageControl: UIPageControl!
+    var index = 0;
     
     // Danh sách ảnh của Slider
-    let bannerImages = ["image1", "image2", "image3"]
+    let bannerImages = ["image1", "image2", "image3", "image4", "image5"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +25,35 @@ class HomeController: UIViewController {
         // Cấu hình page control
         bannerSlidePageControl.numberOfPages = bannerImages.count
         bannerSlidePageControl.currentPage = 0
+        
+        // Xử lý tự động chạy slide sau mỗi 3s
+        Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(scrollingSetup), userInfo: nil, repeats: true)
+        
+        // Xử lý sự kiện khi chạm vào các page item của Page Control
+        bannerSlidePageControl.addTarget(self, action: #selector(pageControlValueChange(_:)), for: .valueChanged)
+    }
+    
+    // Hàm xử lý thay đổi giá trị của page control
+    @objc func pageControlValueChange(_ sender: UIPageControl) {
+        // Cập nhật lại vị trí index của slide
+        index = sender.currentPage
+        
+        // Thực hiện cập nhật lại slide
+        bannerSlidePageControl.currentPage = index
+        bannerSlideCollectionView.scrollToItem(at: IndexPath(item: index, section: 0), at: .right, animated: true)
+    }
+    
+    // Hàm xử lý scrolling
+    @objc func scrollingSetup() {
+        if index < bannerImages.count - 1 {
+            index = index + 1
+        }else {
+            index = 0
+        }
+        
+        // Thực hiện cập nhật lại slide
+        bannerSlidePageControl.currentPage = index
+        bannerSlideCollectionView.scrollToItem(at: IndexPath(item: index, section: 0), at: .right, animated: true)
     }
 }
 
