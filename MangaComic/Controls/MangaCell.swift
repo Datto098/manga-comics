f93e49c7208d677cc20dcb0a6605b504879a5b02
Your Name <your-email@example.com>
2024-05-25 06:12:22 -0700

    init

---
+++ MangaComic/Controls/MangaCell.swift
@@ -0,0 +1,23 @@
+//
+//  MangaCell.swift
+//  MangaComic
+//
+//  Created by tandev on 24/5/24.
+//
+
+import UIKit
+
+class MangaCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
